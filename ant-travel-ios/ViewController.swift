e5655103061034af6a53a3d1df6812a5524df943
xiaoheng <8047107@qq.com>
2015-12-23 15:24:32 +0800

    initial

---
+++ ant-travel-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ant-travel-ios
+//
+//  Created by 肖恒 on 15/12/23.
+//  Copyright © 2015年 xiaoheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ant-travel-iosTests/ant_travel_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  ant_travel_iosTests.swift
+//  ant-travel-iosTests
+//
+//  Created by 肖恒 on 15/12/23.
+//  Copyright © 2015年 xiaoheng. All rights reserved.
+//
+
+import XCTest
+@testable import ant_travel_ios
+
+class ant_travel_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
