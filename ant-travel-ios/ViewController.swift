1b7684a87c6447cf1a369250a33e1eec0e206e4d
xiaoheng <8047107@qq.com>
2015-12-23 18:33:34 +0800

    test pic

--- ant-travel-ios/ViewController.swift
+++ ant-travel-ios/ViewController.swift
@@ -7,19 +7,25 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 设置图片
+        let url = NSURL(string: "http://e.hiphotos.baidu.com/zhidao/pic/item/8c1001e93901213f348294cb56e736d12f2e9570.jpg")
+        imageView.sd_setImageWithURL(url)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
