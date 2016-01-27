2aea9202e31778037811ce303d3cee0fbae04236
xiaoheng <8047107@qq.com>
2016-01-28 00:01:43 +0800

    main

--- ant-travel-ios/Classes/Main/MainViewController.swift
+++ ant-travel-ios/Classes/Main/MainViewController.swift
@@ -13,28 +13,10 @@ class MainViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
-        
-        // 1.添加子控制器
-        /*
-        // 1.1创建子控制器
-        let homeVC = HomeTableViewController()
-        homeVC.tabBarItem.image = UIImage(named: "tabbar_home")
-        homeVC.tabBarItem.selectedImage = UIImage(named: "tabbar_home_highlighted")
-        homeVC.tabBarItem.title = "首页"
-        
-        // 1.2包装一个导航控制器
-        let nav = UINavigationController(rootViewController: homeVC)
-        
-        // 1.3将导航控制器添加到UITabBarController
-        addChildViewController(nav)
-        // 注意点: 在iOS7以前, 如果设置UITabBar的tintColor只有文字有效, 而图片无效
-        //        homeVC.tabBarController?.tabBar.tintColor = UIColor.orangeColor()
-        */
         addChildViewController(HomeTableViewController(), imageName: "tabbar_home", title: "首页")
-//        addChildViewController(MessageTableViewController(), imageName: "tabbar_message_center", title: "消息")
-//        addChildViewController(DiscoverTableViewController(), imageName: "tabbar_discover", title: "广场")
-//        addChildViewController(ProfileTableViewController(), imageName: "tabbar_profile", title: "我")
+        addChildViewController(MessageTableViewController(), imageName: "tabbar_message_center", title: "消息")
+        addChildViewController(DiscoverTableViewController(), imageName: "tabbar_discover", title: "广场")
+        addChildViewController(MeTableViewController(), imageName: "tabbar_profile", title: "我")
     }
     
     func addChildViewController(childController: UIViewController, imageName: String, title: String) {
@@ -42,8 +24,6 @@ class MainViewController: UITabBarController {
         // 1.1创建子控制器
         childController.tabBarItem.image = UIImage(named: imageName)
         childController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
-        //        childController.tabBarItem.title = title
-        //        childController.navigationItem.title = title
         childController.title = title
         
         // 1.2包装一个导航控制器
